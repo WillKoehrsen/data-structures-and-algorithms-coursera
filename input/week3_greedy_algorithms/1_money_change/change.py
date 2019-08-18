6570cdd536f0a8796e134df3444d219e2d582d29
Will K <wjk68@case.edu>
2019-08-18 05:51:06 -0400

    Working on week 3 algorithms

--- input/week3_greedy_algorithms/1_money_change/change.py
+++ input/week3_greedy_algorithms/1_money_change/change.py
@@ -2,8 +2,10 @@
 import sys
 
 def get_change(m):
-    #write your code here
-    return m
+    tens = m // 10
+    leftover = m % 10
+    fives = leftover // 5
+    return tens + fives + leftover % 5
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
--- input/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ input/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -4,7 +4,21 @@ import sys
 def get_optimal_value(capacity, weights, values):
     value = 0.
     # write your code here
-
+    unit_values = [v/w for w, v in zip(weights, values)]
+    tup_values = [(u, w, v) for u, w, v in zip(unit_values, weights, values)]
+    total_weight = 0
+    while total_weight < capacity:
+        if not tup_values:
+            break
+        available_weight = capacity - total_weight
+        max_unit_value, max_weight, max_value = max(tup_values)
+        if max_weight <= available_weight:
+            total_weight += max_weight
+            value += max_value
+        else:
+            total_weight += available_weight
+            value += max_unit_value * available_weight
+        tup_values.remove((max_unit_value, max_weight, max_value))
     return value
 
 
