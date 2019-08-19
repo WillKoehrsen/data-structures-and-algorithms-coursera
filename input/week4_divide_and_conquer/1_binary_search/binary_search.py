f251bc304753f480f2385da1d26b8303895d2ead
Will K <wjk68@case.edu>
2019-08-18 20:52:00 -0400

    Working on Week 4

--- input/week4_divide_and_conquer/1_binary_search/binary_search.py
+++ input/week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -3,7 +3,7 @@ import sys
 
 def binary_search(a, x):
     left, right = 0, len(a)
-    # write your code here
+    
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -14,7 +14,6 @@ def linear_search(a, x):
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
-    
     m = data[n + 1]
     a = data[1 : n + 1]
     for x in data[n + 2:]:
